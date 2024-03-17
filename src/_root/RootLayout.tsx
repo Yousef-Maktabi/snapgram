8c4280dedc886ff7681ea6e4998833d3ed6404a8
Yousef.Maktabi <yousef.maktabii@gmail.com>
2024-03-17 12:46:35 +0330

    feat(home): Add layout and components

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import TopBar from "@/components/shared/TopBar";
+import BottomBar from "@/components/shared/BottomBar";
 
 const RootLayout = () => {
   return (
-    <div>RootLayout</div>
-  )
-}
+    <div className="w-full md:flex">
+      <TopBar />
+      <LeftSideBar />
 
-export default RootLayout
+      <section className="flex flex-1 h-full">
+        <Outlet />
+      </section>
+
+      <BottomBar />
+    </div>
+  );
+};
+
+export default RootLayout;
